040cd40a2b9163a43c1f20ee1d5046f59986dfac
Victor Orsolan <vorsolan@hotmail.com>
2020-09-07 18:55:20 -0300

    Validate model animal andorinha rule

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,15 @@
 class Animal < ApplicationRecord
   belongs_to :person
   enum animal_type: [:cavalo , :cachorro , :papagaio, :lhama, :iguana, :ornitorrinco, :gato, :andorinha]
+  validates :person_id , presence: true
+  validates :person_age , numericality: {greater_than_or_equal_to: 18}, on: :create , if: :swallow_type?
+
+  def swallow_type?
+    self.animal_type == "andorinha"
+  end
+
+  def person_age
+    ((Time.zone.now - self.person.birth_date.to_time) / 1.year.seconds).floor
+  end
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,8 @@
 class Person < ApplicationRecord
+  has_many :animal , dependent: :destroy
+  validates :name , uniqueness: true
+
+  def age
+    ((Time.zone.now - self.birth_date.to_time) / 1.year.seconds).floor
+  end
 end
