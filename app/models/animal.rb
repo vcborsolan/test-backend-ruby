73443006d0be2c5b2f0a3b9d86e4e4661c296739
Victor Orsolan <vorsolan@hotmail.com>
2020-09-08 15:36:34 -0300

    validates person name and person montly cost

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,13 +3,27 @@ class Animal < ApplicationRecord
   enum animal_type: [:cavalo , :cachorro , :papagaio, :lhama, :iguana, :ornitorrinco, :gato, :andorinha]
   validates :person_id , presence: true
   validates :person_age , numericality: {greater_than_or_equal_to: 18}, on: :create , if: :swallow_type?
+  validates :person_name, format: { without: /\A[Aa]{1}/,message: "People cannot adopt cats if the first letter of the name is A"} , on: :create, if: :cat_type?
+  validates :person_mc, numericality: {less_than: 1000 , message: "People cannot adopt animals when total monthly cost is bigger than 1000"}, on: :create
 
   def swallow_type?
     self.animal_type == "andorinha"
   end
 
+  def cat_type?
+    self.animal_type == "gato"
+  end
+
   def person_age
-    ((Time.zone.now - self.person.birth_date.to_time) / 1.year.seconds).floor
+    self.person.age
+  end
+
+  def person_name
+    self.person.name
+  end
+
+  def person_mc
+    self.person.total_mc
   end
 
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -5,4 +5,8 @@ class Person < ApplicationRecord
   def age
     ((Time.zone.now - self.birth_date.to_time) / 1.year.seconds).floor
   end
+
+  def total_mc
+    self.animal.sum(:mc)
+  end
 end
